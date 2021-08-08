b76d68f85b9b3755a96e057596a7486d925eae90
Evaldas Pocius <evaldukas96@gmail.com>
2021-08-08 17:24:38 +0300

    all app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { CssBaseline, Grid } from "@material-ui/core";
+import { getPlacesData } from "./api";
 
-function App() {
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+
+const App = () => {
+  const [places, setPlaces] = useState([]);
+
+  const [cordinates, setCordinates] = useState({});
+  const [bounds, setBounds] = useState({});
+  const [childClicked, setchildClicked] = useState(null);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [type, setType] = useState("restaurants");
+  const [rating, setRating] = useState("");
+  const [filteredPlaces, setfilteredPlaces] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        setCordinates({ lat: latitude, lng: longitude });
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    const filteredPlaces = places?.filter((place) => place.rating > rating);
+
+    setfilteredPlaces(filteredPlaces);
+  }, [rating]);
+
+  useEffect(() => {
+    if (bounds.sw && bounds.ne) {
+      setIsLoading(true);
+      console.log(type);
+
+      getPlacesData(type, bounds.sw, bounds.ne).then((data) => {
+        setPlaces(data?.filter((place) => place.name && place.num_reviews > 0));
+        setIsLoading(false);
+        setfilteredPlaces([]);
+      });
+    }
+  }, [type, bounds]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <Header setterCordinates={setCordinates} />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List
+            places={filteredPlaces?.length ? filteredPlaces : places}
+            childClicked={childClicked}
+            isLoading={isLoading}
+            type={type}
+            setType={setType}
+            rating={rating}
+            setRating={setRating}
+          />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map
+            setCordinates={setCordinates}
+            setBounds={setBounds}
+            cordinates={cordinates}
+            places={filteredPlaces?.length ? filteredPlaces : places}
+            setchildClicked={setchildClicked}
+          />
+        </Grid>
+      </Grid>
+    </>
   );
-}
+};
 
 export default App;
